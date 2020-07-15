cf568c24ab8e5a3440ba26e4bdac0e021d6d3a62
sandrahy <sandra.hernandez.y@usach.cl>
2020-07-15 12:02:34 -0400

    Avance vista recomendados

--- vivlio-front/scripts/main.js
+++ vivlio-front/scripts/main.js
@@ -1,1 +1,13 @@
-const searchBox = document.querySelector(".search-box");
+new Vue({
+  el: '#app',
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('http://192.168.99.102:8000/api/genres/list-all')
+      .then(response => (this.info = response.data.genres))
+      }
+})
