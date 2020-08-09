038731faa9f1ea3817a3157bf3a9d584b1641ef2
HectorPerezM <hector.perez.m@usach.cl>
2020-08-09 13:00:11 -0400

    Componente Navbar, completado registro

---
+++ front-vivlio/src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="nav" class="nav-container">
+        <div class="wrapper">
+            <nav>
+                <div class="logo">
+                    <router-link to="/">
+                        <img alt="Vivlio logo" src="@/assets/logo.png" />
+                    </router-link>
+                </div>
+                
+                <!-- Links for Home page -->
+                <div v-if="this.$route.name ==  'Home'">
+                    <ul class="nav-items">
+                        <li><router-link to="/signUp">Regístrate</router-link></li>
+                        <li><router-link to="/login">Ingresar</router-link></li>
+                    </ul>
+                </div>
+
+                <!-- Links for Signup page -->
+                <div v-if="this.$route.name ==  'SignUp'">
+                    <ul class="nav-items">
+                        <li><router-link to="/">Inicio</router-link></li>
+                        <li><router-link to="/login">Ingresar</router-link></li>
+                    </ul>
+                </div>
+
+            </nav>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+};
+</script>
+
+<style scoped>
+    .nav-items li a {
+        text-decoration: none;
+        font-size: 25px;
+        color: #1c4053;
+    }
+</style>
--- front-vivlio/src/views/SignUp.vue
+++ front-vivlio/src/views/SignUp.vue
@@ -1,78 +1,69 @@
 <template>
   <div class="signUp">
-    <div class="nav-container">
-      <div class="wrapper">
-        <nav>
-          <div class="logo">
-            <router-link to="/">
-            <img alt="Vue logo" src="@/assets/logo.png">
-            </router-link>
-          </div>
-          <ul class="nav-items">
-            <li><router-link to="/login">Ingresar</router-link></li>
-         </ul>
-        </nav>
-      </div>
-    </div>
+    <Navbar />
 
     <div class="body-container">
-            <div class="profile-container">
-                <h1 class="profile-title">Bienvenido a Vivlio</h1>
-                <div class="profile-form">
-                    <div class="profile-title-input">
-                      <p>Nombre</p>
-                    </div>
-                    <div class="profile-input">
-                      <input id="name" v-model="name" type="text" placeholder="   Juan">
-                    </div>
-                    <div class="profile-title-input">
-                      <p>Apellido</p>
-                    </div>
-                    <div class="profile-input">
-                      <input id="lastname" v-model="lastname" type="text" placeholder="   Pérez">
-                    </div>
-                    <div class="profile-title-input">
-                      <p>Email</p>
-                    </div>
-                    <div class="profile-input">
-                      <input id="email" v-model="email" type="email" placeholder="   juan.perez@email.com">
-                    </div>
-                    <div class="profile-title-input">
-                      <p>Contraseña</p>
-                    </div>
-                    <div class="profile-input">
-                      <input id="pwd" v-model="pwd" type="password" placeholder="   ***********">
-                    </div>
-                    <div class="profile-genre-title">
-                      <p>Selecciona cuáles son los géneros literarios que <br>más te gustan:</p>
-                    </div>
+      <div class="profile-container">
+        <h1 class="profile-title">Bienvenido a Vivlio</h1>
+        <div class="profile-form">
+          <div class="profile-title-input">
+            <p>Nombre</p>
+          </div>
+          <div class="profile-input">
+            <input id="name" v-model="name" type="text" placeholder="Juan">
+          </div>
 
-                    <div class="profile-genre-all">
-                      <div v-for="(genre,index) in genres">
-                        <div v-on:click="addGenre(genre, index)" v-if="!isActive[index]" class="profile-genre">
-                          <p>{{genre}}</p>
-                        </div>
-                        <div v-on:click="addGenre(genre, index)" v-else class="profile-genre-selected">
-                          <p>{{genre}}</p>
-                        </div>
-                      </div>
-                    </div>
+          <div class="profile-title-input">
+            <p>Apellido</p>
+          </div>
+          <div class="profile-input">
+            <input id="lastname" v-model="lastname" type="text" placeholder="Pérez">
+          </div>
+          
+          <div class="profile-title-input">
+            <p>Email</p>
+          </div>
+          <div class="profile-input">
+            <input id="email" v-model="email" type="email" placeholder="juan.perez@email.com">
+          </div>
+          
+          
+          <div class="profile-title-input">
+            <p>Contraseña</p>
+          </div>
+          <div class="profile-input">
+            <input id="pwd" v-model="pwd" type="password" placeholder="***********">
+          </div>
+          
+          <div class="profile-genre-title">
+            <p>Indicanos tus preferencias de géneros literarios,<br/> de 1 (poco) a 100 (mucho):</p>
+          </div> 
 
-                    <div class="profile-submit">
-                      <button v-on:click="sendData()">Registrarse</button>
-                    </div>
-
-                </div>
+          <div class="preferences">
+            <div class="preference" v-for="(genre,index) in genres" :key=index>
+              <label class="preference-label" for=index>{{genre}}: {{preferences[index]}}%</label>
+              <input class="preference-range" v-model="preferences[index]" id="index" type="range" min="1" value="10" step="1" max="100" />
             </div>
-        </div>
+          </div>
+
+          <div class="profile-submit">
+            <button v-on:click="sendData()">Registrarse</button>
+          </div>
+
+          </div>
       </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
-
+  import Navbar from '@/components/Navbar.vue'
   import axios from 'axios'
   export default{
+    name: 'SignUp',
+    components: {
+      Navbar
+    },
     data(){
       return {
        theme: ['profile-genre', 'profile-genre', 'profile-genre', 'profile-genre', 'profile-genre', 'profile-genre', 'profile-genre'],
@@ -82,6 +73,7 @@
        email: "",
        pwd: "",
        genres: [],
+       preferences: [],
        selectedGenres: []
      }
     },
@@ -111,14 +103,13 @@
             console.log(this.theme);
         },
         sendData: function () {
-
             let user = {
                 "rut": "1111111",
                 "first_name": this.name,
                 "last_name": this.lastname,
                 "email": this.email,
                 "password": this.pwd,
-                "list_of_tags": this.selectedGenres
+                "preferences": this.preferences
             }
 
             console.log(user);
@@ -131,17 +122,15 @@
             //     })
         },
         getGenres: function () {
-            axios.get('http://192.168.99.100:8000/api/tags/list-tags')
+            axios.get('http://0.0.0.0:8000/api/tags/list-tags')
                 .then(res => {
                     console.log(res);
                     if(res.status === 200) {
                         for (let [_, value] of Object.entries(res.data)) {
                             this.genres.push(value["name "]);
-                            console.log(value["name "]);
+                            this.preferences.push("50");
                         }
                     }
-
-                    console.log(this.genres);
                 })
                 .catch(error => {
                     console.log(error);
@@ -154,3 +143,92 @@
  }
 
 </script>
+
+<style scoped>
+  input[type=text], select {
+    width: 81%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+  }
+  
+  input[type=email], select {
+    width: 81%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+  }
+
+  input[type=password], select {
+    width: 81%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+  }
+  
+  .preferences {
+    color: #1C4053;
+    width: 75%;
+    margin: 0 auto;
+    align-content: center;
+    font-size: 25px;
+    background-color: #f1f1f1;
+  }
+
+  .preference {
+    margin: 5% 5%;
+  }
+
+  .preference-label {
+    padding-right: 40px;
+    text-align: start;
+    font-weight: 300;
+    width: 100%;
+  }
+  
+  .preference-range {
+    background-color: #f1f1f1;
+  }
+
+  input[type=range] {
+    height: 26px;
+    -webkit-appearance: none;
+    margin: 10px 0;
+    width: 100%;
+  }
+
+  input[type=range]:focus {
+    outline: none;
+  }
+
+  input[type=range]::-moz-range-track {
+    width: 100%;
+    height: 4px;
+    cursor: pointer;
+    animate: 0.2s;
+    box-shadow: 1px 1px 1px #000000;
+    background: #1C4053;
+    border-radius: 4px;
+    border: 1px solid #000000;
+  }
+  
+  input[type=range]::-moz-range-thumb {
+    box-shadow: 1px 1px 1px #1C4053;
+    border: 3px solid #1C4053;
+    height: 16px;
+    width: 16px;
+    border-radius: 16px;
+    background: #fff;
+    cursor: pointer;
+  }
+
+</style>
